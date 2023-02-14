2ca017755c74587d13143550f556352f86cbfbfd
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-14 23:51:43 +0300

    UpdateImage Dersinin uygulaması

--- app.mjs
+++ app.mjs
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import fileUpload from 'express-fileupload';
+import methodOverride from 'method-override';
 import path from 'path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 import ejs from 'ejs';
 import fs from 'fs';
 
@@ -32,6 +33,7 @@ app.use(express.static('public')); //We specify the location of static files
 app.use(express.urlencoded({ extended: true })); //encode from url
 app.use(express.json()); //converting the code to json format
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 //Routes
 app.get('/', async (req, res) => {
@@ -40,6 +42,7 @@ app.get('/', async (req, res) => {
     photos,
   });
 });
+
 app.get('/photos/:id', async (req, res) => {
   //console.log(req.params.id)
   const photo = await Photo.findById(req.params.id);
@@ -47,18 +50,28 @@ app.get('/photos/:id', async (req, res) => {
     photo,
   });
 });
+//Urledeki id'yi alıyor.
+//Veritabanında ona denk gelen id'yi photo değişkenine atıyor
+//Değişken edit sayfasına photo değişkeni ile yollanıyor.
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
 app.get('/add', (req, res) => {
   res.render('add');
 });
+
 app.post('/photos', async (req, res) => {
   //console.log(req.files.image)
   //await Photo.create(req.body);
   //res.redirect('/'); //Redirects to homepage
-
-
   //Eğer klasör yoksa uploads klasörü oluşturacak.
   const uploadDir = 'public/uploads';
   if (!fs.existsSync(uploadDir)) {
@@ -68,13 +81,35 @@ app.post('/photos', async (req, res) => {
   let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
 
   uploadeImage.mv(uploadPath, async (err) => {
-    if(err) console.log(err);
+    if (err) console.log(err);
     await Photo.create({
       ...req.body,
       image: '/uploads/' + uploadeImage.name,
     });
-  }); 
+  });
   res.redirect('/');
+});
+
+//findOne metoduyla id bilgisi sayesinde filtreleme yaptık.
+//Elde ettiğimiz sql satırının title,description gibi alanlarında değişiklik yaptık
+//Fotoğraf güncellemesi için
+//
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  //Form ile iletilen fotoğrafın dosya bilgilerini aldım.
+  let uploadeImage = req.files.image;
+  //Formdan alınan fotoğrafları kaydedeceğim yerin dosya dizinini değişkene atadım.
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+  //mv metodu ile belirttiğim dosya dizinine taşıdım. 
+  uploadeImage.mv(uploadPath);
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  //Veri tabanında kaydettiğimiz uzantıda değişiklik yaptım.
+  photo.image = '/uploads/' + uploadeImage.name;
+  //Çektiğim fineOne sorgusunda yaptığım değişiklikleri kayıt ettim.
+  photo.save();
+  //Görsele özel olan sayfaya yönlendirir.
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 const port = 3000;
